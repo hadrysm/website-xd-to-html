9d76548f64fd621b8467802e3bb5929943a49ba0
Mateusz Hadrys <m.hadrys.95@gmail.com>
2020-05-01 21:03:00 +0200

    add debounce on scroll

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
 import '../scss/index.scss';
 
 import { burger, toogleNav } from './menu';
-import { scroll } from './scroll';
+import { debounce, scroll } from './scroll';
 
 const init = () => {
-    burger.addEventListener('click', toogleNav);
-    window.addEventListener('scroll', scroll);
+  burger.addEventListener('click', toogleNav);
+  window.addEventListener('scroll', debounce(scroll));
 };
 
 window.addEventListener('DOMContentLoaded', init);
--- src/js/scroll.js
+++ src/js/scroll.js
@@ -5,39 +5,56 @@ const projekts = document.querySelectorAll('.projekt__text');
 const images = document.querySelectorAll('.projekt__picture');
 const boxes = document.querySelectorAll('.box');
 
+export const debounce = (func, wait = 18, immediate = true) => {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
 export const scroll = () => {
-    const scroll = window.scrollY;
+  console.log('scrol');
+  const scroll = window.scrollY;
 
-    if (scroll > 100) {
-        header.classList.add('header--scroll');
-    } else {
-        header.classList.remove('header--scroll');
+  if (scroll > 100) {
+    header.classList.add('header--scroll');
+  } else {
+    header.classList.remove('header--scroll');
+  }
+
+  projekts.forEach(projekt => {
+    const distanceFromTop = projekt.getBoundingClientRect().top;
+
+    if (distanceFromTop < viewHeight - 100) {
+      projekt.classList.add('fade-in');
     }
+  });
 
-    projekts.forEach(projekt => {
-        const distanceFromTop = projekt.getBoundingClientRect().top;
+  images.forEach(image => {
+    const distanceFromTop = image.getBoundingClientRect().top;
 
-        if (distanceFromTop < viewHeight - 100) {
-            projekt.classList.add('fade-in');
-        }
-    });
+    if (distanceFromTop < viewHeight - 100) {
+      image.classList.add('fade-in');
+      image.style.animationName = 'fadeIn';
+    }
+  });
 
-    images.forEach(image => {
-        const distanceFromTop = image.getBoundingClientRect().top;
+  boxes.forEach(box => {
+    const distanceFromTop = box.getBoundingClientRect().top;
 
-        if (distanceFromTop < viewHeight - 100) {
-            image.classList.add('fade-in');
-            image.style.animationName = 'fadeIn';
-        }
-    });
-
-    boxes.forEach(box => {
-        const distanceFromTop = box.getBoundingClientRect().top;
-
-        if (distanceFromTop < viewHeight - 100) {
-            box.classList.add('fade-in');
-            box.style.animationName = 'fadeInScale';
-            box.style.animationDuration = '.5s';
-        }
-    });
+    if (distanceFromTop < viewHeight - 100) {
+      box.classList.add('fade-in');
+      box.style.animationName = 'fadeInScale';
+      box.style.animationDuration = '.5s';
+    }
+  });
 };
